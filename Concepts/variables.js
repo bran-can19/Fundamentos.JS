4659f21530150578b88c27163eaefd4d0710be40
lata-de-salvado19 <candanedabrandon@gmail.com>
2024-09-12 13:57:39 -0600

    03 Variables let y const

---
+++ Concepts/variables.js
@@ -0,0 +1,18 @@
+// DECLARACIÓN DE VARIABLES CON LET Y CONST
+// VAR: en desuso
+
+//variables  let: cambiarán de valor
+let age; // declaración
+console.log(age);
+age = 23; // inicialización
+console.log(age);
+
+let name = "Francisco"; // declaración d inicializacióm
+console.log(name);
+name = "Brandon";
+console.log(name);
+
+// constante const: no cambiaran de valor
+const PI = 3.1416;
+//PI = 3.15;
+console.log(PI);
